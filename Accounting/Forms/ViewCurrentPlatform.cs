195e6771c1a1b2e6fd1cdbd86c12522075a837ea
Roman Sivak <roman.sivak@bluecieloecm.com>
2019-04-09 10:11:00 +0300

    New view

---
+++ Accounting/Forms/ViewCurrentPlatform.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Accounting.Forms
+{
+    public partial class ViewCurrentPlatform : Form
+    {
+        protected long PlatformId;
+        public ViewCurrentPlatform(long platformId)
+        {
+            PlatformId = platformId;
+            InitializeComponent();
+        }
+
+        private void ViewCurrentPlatform_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
